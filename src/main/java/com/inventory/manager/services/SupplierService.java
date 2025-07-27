0e8d8abdbbd9fc5b85a6d5f93541a9a5d73cfccf
MGuerra50 <matheusguerra50@gmail.com>
2025-07-27 14:53:21 -0300

    feat: supplier crud

---
+++ src/main/java/com/inventory/manager/services/SupplierService.java
@@ -0,0 +1,61 @@
+package com.inventory.manager.services;
+
+import com.inventory.manager.domain.supplier.Supplier;
+import com.inventory.manager.domain.supplier.SupplierDTO;
+import com.inventory.manager.domain.supplier.SupplierRequestDTO;
+import com.inventory.manager.domain.supplier.SupplierRespository;
+import com.inventory.manager.exception.ResourceNotFoundException;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class SupplierService {
+
+    @Autowired
+    SupplierRespository supplierRespository;
+
+    public Supplier findById (Long id){
+        return supplierRespository.findById(id).orElseThrow(
+                ()-> new ResourceNotFoundException("Supplier id \"" + id + "\" não encontrado.")
+        );
+    }
+
+    public List<SupplierDTO> findAll (){
+        return supplierRespository.findAll().stream().map(SupplierDTO::new).collect(Collectors.toList());
+    }
+
+    public SupplierDTO createSupplier (SupplierRequestDTO supplierRequestDTO){
+        Supplier supplier = new Supplier();
+        supplier.setFantasyName(supplierRequestDTO.fantasyName());
+        supplier.setCompanyName(supplierRequestDTO.companyName());
+        supplier.setCnpj(supplierRequestDTO.cnpj());
+        supplier.setIsActive(true);
+        Supplier savedSupplier = supplierRespository.save(supplier);
+        return new SupplierDTO(savedSupplier);
+    }
+
+    @Transactional
+    public SupplierDTO updateSupplier (Long id, SupplierDTO supplierDTO){
+        Supplier supplier = findById(id);
+        if (supplier == null) throw new ResourceNotFoundException("Supplier id \"" + id + "\" não encontrado.");
+
+        supplier.setFantasyName(supplierDTO.fantasyName());
+        supplier.setCompanyName(supplierDTO.companyName());
+        supplier.setCnpj(supplierDTO.cnpj());
+        supplier.setIsActive(supplierDTO.isActive());
+
+        Supplier savedSupplier = supplierRespository.save(supplier);
+        return new SupplierDTO(savedSupplier);
+    }
+
+    public void deleteById (Long id){
+        if(!supplierRespository.existsById(id))
+            throw new ResourceNotFoundException("Suppliers id \"" + id + "\" não encontrado.");
+
+        supplierRespository.deleteById(id);
+    }
+}
