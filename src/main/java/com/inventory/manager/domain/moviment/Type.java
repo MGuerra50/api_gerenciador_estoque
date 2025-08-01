50aa3687ae6aa8ebbdb3415a2b3bcd0c67801673
MGuerra50 <matheusguerra50@gmail.com>
2025-08-01 14:34:56 -0300

    feat: using lombok annotation

--- src/main/java/com/inventory/manager/domain/moviment/Type.java
+++ src/main/java/com/inventory/manager/domain/moviment/Type.java
@@ -1,18 +1,18 @@
 package com.inventory.manager.domain.moviment;
 
+import lombok.Getter;
+
+@Getter
 public enum Type {
     INPUT("imput"),
     OUTPUT("output"),
     ADJUSTMENT("adjustment"),
     TRANSFER("transfer");
 
-    private String type;
+    private final String type;
 
     Type(String type){
         this.type = type;
     }
 
-    public String getType (){
-        return type;
-    }
 }
