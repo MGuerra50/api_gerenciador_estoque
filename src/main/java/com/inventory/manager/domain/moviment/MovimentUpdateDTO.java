c72635cdc24bc79cc3ccb4d656b2669078f74ec7
MGuerra50 <matheusguerra50@gmail.com>
2025-08-04 09:08:39 -0300

    feat: finished crud moviment

--- src/main/java/com/inventory/manager/domain/moviment/MovimentUpdateDTO.java
+++ src/main/java/com/inventory/manager/domain/moviment/MovimentUpdateDTO.java
@@ -4,7 +4,6 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 
-public record MovimentUpdateDTO(@NotBlank Type type,
-                                @NotNull @Positive int numberOfItemsTransaction,
-                                @NotNull Boolean isActive) {
+public record MovimentUpdateDTO(@NotNull Type type,
+                                @NotNull @Positive int numberOfItemsTransaction) {
 }
